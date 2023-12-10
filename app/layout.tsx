dfc067f6d00fa8754c1b5d6acea6ce816e82ee87
Nils Westgårdh <72079716+NilsWestgardh@users.noreply.github.com>
2023-12-10 09:26:22 +0100

    Add dependencies

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,7 @@
-import { GeistSans } from 'geist/font/sans'
+import { Inter } from 'next/font/google'
 import './globals.css'
+
+const inter = Inter({ subsets: ['latin'] })
 
 const defaultUrl = process.env.VERCEL_URL
   ? `https://${process.env.VERCEL_URL}`
@@ -7,8 +9,8 @@ const defaultUrl = process.env.VERCEL_URL
 
 export const metadata = {
   metadataBase: new URL(defaultUrl),
-  title: 'Next.js and Supabase Starter Kit',
-  description: 'The fastest way to build apps with Next.js and Supabase',
+  title: 'Nexus TCG',
+  description: 'An AI-enabled digital TCG where players can make their own cards.',
 }
 
 export default function RootLayout({
@@ -17,7 +19,7 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en" className={GeistSans.className}>
+    <html lang="en" className={inter.className}>
       <body className="bg-background text-foreground">
         <main className="min-h-screen flex flex-col items-center">
           {children}
