363efdb05ae9441d931f0019b277852c6c4a92d0
Nils Westgårdh <72079716+NilsWestgardh@users.noreply.github.com>
2023-12-14 14:16:30 +0100

    commit

--- app/styles/theme.ts
+++ app/styles/theme.ts
@@ -74,56 +74,6 @@ export const theme: ThemeOptions = {
       fontWeight: 400,
     },
   },
-  components: {
-    MuiButton: {
-      styleOverrides: {
-        root: {
-          background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
-          border: 0,
-          borderRadius: 3,
-          boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
-          color: 'white',
-          height: 48,
-          padding: '0 30px',
-        },
-      },
-    },
-    MuiSwitch: {
-      styleOverrides: {
-        root: {
-          width: 46,
-          height: 27,
-          padding: 0,
-          margin: 8,
-        },
-        switchBase: {
-          padding: 1,
-          '&$checked, &$colorPrimary$checked, &$colorSecondary$checked': {
-            transform: 'translateX(16px)',
-            color: '#fff',
-            '& + $track': {
-              opacity: 1,
-              border: 'none',
-            },
-          },
-        },
-        thumb: {
-          width: 24,
-          height: 24,
-        },
-        track: {
-          borderRadius: 13,
-          border: '1px solid #bdbdbd',
-          backgroundColor: '#fafafa',
-          opacity: 1,
-          transition: 'background-color 300ms cubic-bezier(0.4, 0, 0.2, 1) 0ms,border 300ms cubic-bezier(0.4, 0, 0.2, 1) 0ms',
-        },
-      },
-    },
-  },
-  shape: {
-    borderRadius: 4,
-  },
 };
 
 export default theme;
