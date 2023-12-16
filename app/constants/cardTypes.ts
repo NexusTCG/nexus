32fd24b804c66f98381b083b60743bc73f728d14
Nils Westgårdh <72079716+NilsWestgardh@users.noreply.github.com>
2023-12-16 23:58:45 +0100

    commit

--- app/constants/cardTypes.ts
+++ app/constants/cardTypes.ts
@@ -1,11 +1,99 @@
-export const cardTypes = [
+import { ElementType } from 'react';
+import PetsIcon from '@mui/icons-material/Pets';
+import BugReportIcon from '@mui/icons-material/BugReport';
+import CrisisAlertIcon from '@mui/icons-material/CrisisAlert';
+import SolarPowerIcon from '@mui/icons-material/SolarPower';
+
+export type CardType = {
+    id: number;
+    name: string;
+    label: string;
+    description: string;
+    flavor: string;
+    superTypes: string;
+    subTypes: string;
+    isPermanent: boolean;
+    isScript: boolean;
+    icon: ElementType;
+};
+
+export const cardTypes: CardType[] = [
     {
         id: 1,
-        value: "",
+        name: "Entity",
         label: "",
         description: "",
         flavor: "",
+        superTypes: "",
+        subTypes: "",
+        isPermanent: true,
+        isScript: true,
+        icon: PetsIcon,
+    }, {
+        id: 2,
+        name: "Interrupt",
+        label: "",
+        description: "",
+        flavor: "",
+        superTypes: "",
+        subTypes: "",
         isPermanent: false,
+        isScript: true,
+        icon: CrisisAlertIcon,
+    }, {
+        id: 3,
+        name: "",
+        label: "",
+        description: "",
+        flavor: "",
+        superTypes: "",
+        subTypes: "",
+        isPermanent: false,
+        isScript: false,
+        icon: BugReportIcon,
+    }, {
+        id: 4,
+        name: "",
+        label: "",
+        description: "",
+        flavor: "",
+        superTypes: "",
+        subTypes: "",
+        isPermanent: false,
+        isScript: false,
+        icon: BugReportIcon,
+    }, {
+        id: 5,
+        name: "",
+        label: "",
+        description: "",
+        flavor: "",
+        superTypes: "",
+        subTypes: "",
+        isPermanent: false,
+        isScript: false,
+        icon: BugReportIcon,
+    }, {
+        id: 6,
+        name: "",
+        label: "",
+        description: "",
+        flavor: "",
+        superTypes: "",
+        subTypes: "",
+        isPermanent: false,
+        isScript: false,
+        icon: BugReportIcon,
+    }, {
+        id: 7,
+        name: "Source",
+        label: "",
+        description: "",
+        flavor: "",
+        superTypes: "",
+        subTypes: "",
+        isPermanent: false,
+        isScript: false,
+        icon: SolarPowerIcon,
     },
 ];
-  
