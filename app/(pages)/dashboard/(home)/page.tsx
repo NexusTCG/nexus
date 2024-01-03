d99fd5b82b3226cf3cd41ad354120e592ce262f2
Nils Westgårdh <72079716+NilsWestgardh@users.noreply.github.com>
2024-01-03 22:46:48 +0100

    commit

--- app/(pages)/dashboard/(home)/page.tsx
+++ app/(pages)/dashboard/(home)/page.tsx
@@ -1,12 +1,12 @@
 // import NexusCardForm from "@/app/components/card-creator/NexusCardForm";
-// import { Box, Typography } from "@mui/material/";
+import { Box, Typography } from "@mui/material/";
 
 export default function DashboardHome() {
     return (
-        // <Box>
-        //     <Typography variant="h1">Dashboard Home</Typography>
-        //     <NexusCardForm />
-        // </Box>
-        <h1>Dashboard</h1>
+        <Box>
+            <Typography variant="h1">Dashboard Home</Typography>
+            {/* <NexusCardForm /> */}
+        </Box>
+        // <h1>Dashboard</h1>
     );
 };
