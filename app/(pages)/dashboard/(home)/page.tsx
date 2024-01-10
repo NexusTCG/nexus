9dc35c26baee5dd0408687dafddfd40837dabb4a
Nils Westgårdh <72079716+NilsWestgardh@users.noreply.github.com>
2024-01-10 23:43:32 +0100

    commit

--- app/(pages)/dashboard/(home)/page.tsx
+++ app/(pages)/dashboard/(home)/page.tsx
@@ -2,8 +2,15 @@
 
 import NexusCardForm from "@/app/components/card-creator/NexusCardForm";
 import { Box, Typography } from "@mui/material/";
+import readUserSession from "@/app/lib/actions";
+import { redirect } from "next/navigation";
 
-export default function DashboardHome() {
+export default async function DashboardHome() {
+    const {data} = await readUserSession();
+	if (!data.session) {
+		// If there is no session, redirect
+		return redirect("/auth-server-action");
+	}
     return (
         <Box className="flex flex-col w-full h-full gap-4 p-6 bg-gray-800 md:bg-gray-900">
             <Typography variant="h1">Dashboard</Typography>
--- app/auth-server-action/page.tsx
+++ app/auth-server-action/page.tsx
@@ -1,7 +1,15 @@
 import React from "react";
 import { AuthForm } from "./components/AuthForm";
+import readUserSession from "@/app/lib/actions";
+import { redirect } from "next/navigation";
 
-export default function page() {
+export default async function page() {
+	const {data} = await readUserSession();
+	if (data.session) {
+		// If there is a session, redirect
+		return redirect("/dashboard");
+	}
+
 	return (
 		<div className="flex justify-center items-center h-screen">
 			<div className="w-96">
