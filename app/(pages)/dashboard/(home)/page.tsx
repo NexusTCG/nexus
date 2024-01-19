6842b985c9dd705e668c83c95aeb4e43ae9f35a9
Nils Westgårdh <72079716+NilsWestgardh@users.noreply.github.com>
2024-01-19 12:14:13 +0100

    commit

--- app/(pages)/dashboard/(home)/page.tsx
+++ app/(pages)/dashboard/(home)/page.tsx
@@ -1,14 +1,8 @@
 import { Box, Typography } from "@mui/material/";
 import { redirect } from "next/navigation";
 import NexusCardForm from "@/app/components/card-creator/NexusCardForm";
-import readUserSession from "@/app/lib/actions/readSession";
 
 export default async function DashboardHome() {
-    const {data} = await readUserSession();
-	if (!data.session) {
-		// If there is no session, redirect
-		return redirect("/auth-server-action");
-	}
     return (
         <Box className="flex flex-col w-full h-full gap-4 p-6 bg-gray-800 md:bg-gray-900">
             <Typography variant="h1">Dashboard</Typography>
@@ -16,6 +10,3 @@ export default async function DashboardHome() {
         </Box>
     );
 };
-
-// Something is broken here. Getting this error:
-//  ⨯ Internal error: Error: Element type is invalid: expected a string (for built-in components) or a class/function (for composite components) but got: undefined. You likely forgot to export your component from the file it's defined in, or you might have mixed up default and named imports.
--- app/login/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function Dashboard() {
-  return (
-    <form action="submit">
-      <p>Logout</p>
-      <button type="submit">Logout</button>
-      {/* Need to create sign up sign in and sign out options */}
-    </form>
-  ) 
-}
