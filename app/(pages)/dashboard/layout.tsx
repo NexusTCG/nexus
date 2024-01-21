8fde966c4a27a0d785b88277e564b3d4d3c89f3f
Nils Westgårdh <72079716+NilsWestgardh@users.noreply.github.com>
2024-01-21 19:59:06 +0100

    commit

--- app/(pages)/dashboard/layout.tsx
+++ app/(pages)/dashboard/layout.tsx
@@ -1,21 +1,21 @@
-// import { Box } from '@mui/material';
+import { Box } from '@mui/material';
 
-// export default function DashboardLayout({
-//   children,
-// }: {
-//   children: React.ReactNode
-// }) {
-//   return (
-//     <Box className="flex flex-row justify-start items-start w-full">
-//       <Box
-//         sx={{ width: "240px", minHeight: "100vh", position: "sticky", margin: "0px" }}
-//         className="hidden lg:flex flex-col p-6 bg-slate-950 border-r border-slate-800"
-//       >
-//         <Box className="flex w-full h-full bg-slate-800 rounded-lg"></Box>
-//       </Box>
-//       <Box className="flex flex-col justify-center items-center w-full mx-0 md:mx-12 my-12">
-//         {children}
-//       </Box>
-//     </Box>
-//   )
-// }
+export default function DashboardLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <Box className="flex flex-row justify-start items-start w-full">
+      <Box
+        sx={{ width: "240px", minHeight: "100vh", position: "sticky", margin: "0px" }}
+        className="hidden lg:flex flex-col p-6 bg-slate-950 border-r border-slate-800"
+      >
+        <Box className="flex w-full h-full bg-slate-800 rounded-lg"></Box>
+      </Box>
+      <Box className="flex flex-col justify-center items-center w-full mx-0 md:mx-12 my-12">
+        {children}
+      </Box>
+    </Box>
+  )
+}
