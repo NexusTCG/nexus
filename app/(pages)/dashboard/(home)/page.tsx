092c8db2729e85e27977b3a3911ce06c1169593d
Nils Westgårdh <72079716+NilsWestgardh@users.noreply.github.com>
2024-01-21 19:42:39 +0100

    commit

--- app/(pages)/dashboard/(home)/page.tsx
+++ app/(pages)/dashboard/(home)/page.tsx
@@ -1,12 +1,11 @@
-import { Box, Typography } from "@mui/material/";
-import { redirect } from "next/navigation";
-import NexusCardForm from "@/app/components/card-creator/NexusCardForm";
+// import { Box, Typography } from "@mui/material/";
+// import NexusCardForm from "@/app/components/card-creator/NexusCardForm";
 
-export default async function DashboardHome() {
-    return (
-        <Box className="flex flex-col w-full h-full gap-4 p-6 bg-gray-800 md:bg-gray-900">
-            <Typography variant="h1">Dashboard</Typography>
-            <NexusCardForm />
-        </Box>
-    );
-};
+// export default async function DashboardHome() {
+//     return (
+//         <Box className="flex flex-col w-full h-full gap-4 p-6 bg-gray-800 md:bg-gray-900">
+//             <Typography variant="h1">Dashboard</Typography>
+//             <NexusCardForm />
+//         </Box>
+//     );
+// };
