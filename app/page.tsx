c1ff345f0868b2bf3de844f42989ab61f5640d86
Nils Westgårdh <72079716+NilsWestgardh@users.noreply.github.com>
2024-01-22 12:16:21 +0100

    commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,10 @@
-import { supabase } from "@/app/lib/supabase";
+import Link from "next/link";
 
 export default function Home() {
-  const setNewView = async () => {
-    const { data, error } = await supabase
-    .from("views")
-    .insert({
-      name: "new name"
-    })
-
-    if (data) console.log(data);
-    if (error) console.log(error);
-  };
-
-  setNewView();
   return (
-    <main>Logged in</main>
+    <>
+      <h1>Home</h1>
+      <Link href="/login">Login</Link>
+    </>
   )
 }
