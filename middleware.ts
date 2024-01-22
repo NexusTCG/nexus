73265b2fcd3d4f77a53a8f40e00de22819d71ad6
Nils Westgårdh <72079716+NilsWestgardh@users.noreply.github.com>
2024-01-22 21:17:11 +0100

    commit

--- middleware.ts
+++ middleware.ts
@@ -1,71 +1,39 @@
-import { NextResponse, type NextRequest } from "next/server";
+import { NextResponse, NextRequest } from "next/server";
 import { createClient } from "@/app/utils/supabase/middleware";
 
 export async function middleware(request: NextRequest) {
-    
-    // const { supabase, response } = createClient(request);
+    const { supabase, response } = createClient(request);
 
-    // const { data:
-    //     { session }, error } = await supabase
-    //         .auth
-    //         .getSession();
+    const { data:
+        { session }, error } = await supabase
+            .auth
+            .getSession();
 
-    // console.log(`User session: ${session}`);
-    // const path = new URL(request.url).pathname;
+    // console.log(`User session: ${JSON.stringify(session, null, 2)}`);
 
-    // if (error) {
-    //     console.log(error);
-    // }
+    const path = new URL(request.url).pathname;
 
-    // if (session && (
-    //     path === '/' || path === '/login'
-    // )) {
-    //     return NextResponse.redirect(
-    //         new URL('/dashboard', request.url
-    //     ));
-    // }
-
-    // if (!session && path !== '/' && path !== '/login') {
-    //     return NextResponse.redirect(new URL('/login', request.url));
-    // };
-
-    // return response;
-
-    try {
-        const { supabase, response } = createClient(request);
-        const { data:
-            { session }, error } = await supabase
-                .auth
-                .getSession();
-        console.log(`User session: ${session}`);
-        const path = new URL(request.url).pathname;
-
-        // Redirect logged-in users trying to access
-        // '/' or '/login' to '/dashboard'
-        if (session && (
-            path === '/' || path === '/login'
-        )) {
-            return NextResponse.redirect(
-                new URL('/dashboard', request.url
-            ));
-        }
-
-        // If there's no session and the path is not
-        // '/login' or '/', redirect to '/login'
-        if (!session && path !== '/' && path !== '/login') {
-            return NextResponse.redirect(new URL('/login', request.url));
-        }
-
-        return response;
-
-    } catch (error) {
-        console.error(error);
-        return NextResponse.next();
+    if (error) {
+        console.log(error);
     }
+
+    if (session && (
+        path === '/' || path === '/login'
+    )) {
+        return NextResponse.redirect(
+            new URL('/dashboard', request.url
+        ));
+    }
+
+    if (!session && path !== '/' && path !== '/login') {
+        return NextResponse.redirect(new URL('/login', request.url));
+    };
+
+    return response;
 }
 
 export const config = {
     matcher: [
-        "/((?!api|_next/static|_next/image|favicon.ico|login).*)",
+        "/((?!auth|_next/static|_next/image|favicon.ico|login).*)",
     ]
 }
