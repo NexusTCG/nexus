2ec90e01c12f524bb2f5c7c4c1e717cb9199d36b
Nils Westgårdh <72079716+NilsWestgardh@users.noreply.github.com>
2024-02-01 00:36:06 +0100

    commit

--- app/lib/actions/determineColor.ts
+++ app/lib/actions/determineColor.ts
@@ -8,26 +8,40 @@ export default function determineColor(
     const energyCostEntries = Object.entries(activeCardCost ?? {});
 
     const coloredCosts = energyCostEntries
-    .filter(
-        ([color, value]) =>
-        color !== monoColorOptions.void
-        && value > 0
+        .filter(
+            ([color, value]) =>
+            color !== monoColorOptions.void
+            && value > 0
     );
 
     if (!coloredCosts && energyCostEntries.length === 1) {
         return "void";
-    } else if (cardColorType === "mono") {
-        const monoColor = coloredCosts
-            .find(([color]) => color !== monoColorOptions.void);
-        return monoColor ? monoColor[0] : "void";
-    } else if (cardColorType === "dual" && coloredCosts.length === 2) {
-        const activeColors = coloredCosts
-            .map(([color]) => color)
-            .sort();
-        const colorKey = `${activeColors[0]}${activeColors[1]}`;
-        const dualColorKey = Object.keys(dualColorOptions).find(
-            key => key.toLowerCase() === colorKey.toLowerCase()
-        );
+    } else if (
+            cardColorType === "mono"
+        ) {
+            const monoColor = coloredCosts
+                .find(([color]) => color !== monoColorOptions.void);
+            return monoColor ? monoColor[0] : "void";
+    } else if (
+            cardColorType === "dual" &&
+            coloredCosts.length === 2
+        ) {
+            const colorOrder = ["yellow", "blue", "purple", "red", "green"];
+
+            const sortByColorOrder = (colorA: string, colorB: string) => {
+                const indexA = colorOrder.indexOf(colorA);
+                const indexB = colorOrder.indexOf(colorB);
+                return indexA - indexB;
+            };
+
+            const activeColors = coloredCosts
+                .map(([color]) => color)
+                .sort((a, b) => sortByColorOrder(a, b));
+
+            const colorKey = `${activeColors[0]}${activeColors[1]}`;
+            const dualColorKey = Object.keys(dualColorOptions).find(
+                key => key.toLowerCase() === colorKey.toLowerCase()
+            );
         return dualColorKey
             ? dualColorOptions[dualColorKey as keyof typeof dualColorOptions]
             : null;
