21d053b120a9abc075531bf86c0a9d8676b8f709
Nils Westgårdh <72079716+NilsWestgardh@users.noreply.github.com>
2024-02-10 10:41:09 +0100

    commit

--- app/(protected)/dashboard/[test]/card-creator-form/page.tsx
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import CardCreatorForm from "@/app/components/card-creator/CardCreatorForm";
-
-export default function CardCreatorFormTest() {
-  return <CardCreatorForm />;
-}
