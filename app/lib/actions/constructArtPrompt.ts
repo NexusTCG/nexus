4f07dd92311cc7df5b9a2712be0aa03f3c37116c
Nils Westgårdh <72079716+NilsWestgardh@users.noreply.github.com>
2024-02-17 01:40:39 +0100

    commit

--- app/lib/actions/constructArtPrompt.ts
+++ app/lib/actions/constructArtPrompt.ts
@@ -26,16 +26,8 @@ export default async function ConstructArtPrompt(
     const composition = selections.composition ? `Composition: ${selections.composition}` : "";
 
     const promptInstructions = "Generate a piece of art based on the following prompt: ";
-    const promptStart = "Digital painting, illustration, sci-fi, fantasy, rule of thirds, dynamic poses. ";
-    const promptEnd = "DO NOT: Include sexual content, art tools (such as pencils, brushes, etc.), generate characters with the likeness of proprietary characters, violate copyright protected material and intellectual property, generate anything but original artwork."
-    
-    // const promptParts = Object
-    //   .entries(selections)
-    //   // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    //   .map(([category, value]) => {
-    //   return value || "";
-    // }).join(" ");
-
+    const promptStart = "Digital painting, illustration, sci-fi, fantasy, rule of thirds, dynamic poses, centered focus. ";
+    const promptEnd = "DO NOT: Include sexual content, art tools (such as pencils, brushes, etc.), include hands of someone producing the art, user interfaces, generate characters with the likeness of proprietary characters, violate copyright protected material and intellectual property, generate anything but original artwork."
 
     const constructedArtPrompt = `${promptInstructions} ${promptStart} ${style}. ${technique}. ${userPrompt}. ${subject}. ${setting}. ${time}. ${weather}. ${mood}. ${composition}. ${promptEnd}`;
 
