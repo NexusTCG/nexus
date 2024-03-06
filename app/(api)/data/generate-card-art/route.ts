4651c6c41180821f11969e12586c00b4e8e866ef
Nils Westgårdh <72079716+NilsWestgardh@users.noreply.github.com>
2024-03-06 09:58:29 +0100

    commit

--- app/(api)/data/generate-card-art/route.ts
+++ app/(api)/data/generate-card-art/route.ts
@@ -1,5 +1,3 @@
-"use server";
-
 import { NextRequest } from "next/server";
 import { uploadCardArtImage } from "@/app/lib/actions/supabase-data/uploadCardArtImage";
 import fetch from "node-fetch";
